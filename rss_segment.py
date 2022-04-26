a3d628d5a92f4d07d52caaea8067e1326ee51493
talkinglibrary <nashvilletalkinglibrary@gmail.com>
2022-04-26 16:42:49 -0500

    major update to rss_segment
    a whole bunch of updates here.
    
    remove function remove_old_files. this is no longer needed, since the file name stays the same each day. shutil will overwrite existing files.
    
    separated lots of  things out into different functions. cleaner
    
    we're now checking whether the downloaded audio file is an empty file. if it is, we're downloading it again (three times total).
    
    changed some variable names and function names for clarity. also, on function calls, we're more clearly passing the arguments, even if there's only one argument. helps readability.
    
    added global check_if_above and below variables, so user can use custom values.
    
    separated out getting the rss feed into two functions. one gets the PubDate, one the audio URL. this allows us to not use global variables in other functions.

--- rss_segment.py
+++ rss_segment.py
@@ -8,12 +8,12 @@ This is for segments without a date attached.
 © Nashville Public Library
 © Ben Weddle is to blame for this code. Anyone is free to use it.
 '''
+
 import xml.etree.ElementTree as ET
 import subprocess
 from datetime import datetime
 import shutil
 import os
-import glob
 import logging
 import logging.handlers
 from logging.handlers import SysLogHandler
@@ -25,10 +25,14 @@ import urllib.request
 
 #-----change these for each new program-----
 
-show = 'Some Short Daily segment' # for notifications
-filename = 'TLSegmentToAir.wav' #name of file
-url="http://somewebsite.org/somerssfeed.xml" #source rss feed
+show = 'Some Cool TL Segment' # for notifications
+output_file = 'NameOfSegment.wav' #name of file
+url = "http://somesite.org/somesegment.xml" #source rss feed
 
+# these are for checking whether the length (in minutes!) of the file is outside of a range.
+# decimal numbers are OK.
+check_if_above = 0
+check_if_below = 0
 
 #*****----------SHOULD NOT NEED TO CHANGE ANYTHING BELOW THIS LINE----------*****
 
@@ -40,23 +44,41 @@ onair_pc = os.environ["OnAirPC"] #OnAir PC
 short_day = datetime.now().strftime('%a') #used to match pubdate in rss feed, and other purposes.
 timestamp = datetime.now().strftime('%H:%M:%S on %d %b %Y')
 
-def convert_copy():
-    '''download file, convert with ffmpeg, check length, copy to destinations, remove files from this folder'''
-    subprocess.run(f'wget -q -O input.mp3 {download}') #using wget because urlretrive is getting a 403 denied error
-    subprocess.run(f'ffmpeg -hide_banner -loglevel quiet -i input.mp3 -ar 44100 -ac 1 -af loudnorm=I=-21 -y {filename}')
+def convert_copy(input):
+    '''convert with ffmpeg, check length, copy to destinations, remove files from this folder'''
+    subprocess.run(f'ffmpeg -hide_banner -loglevel quiet -i {input} -ar 44100 -ac 1 -af loudnorm=I=-21 -y {output_file}')
     check_length() #check file length before deleting file from current directory
-    shutil.copy(f'{filename}', f'{production_pc}\{filename}')
-    shutil.copy(f'{filename}', f'{onair_pc}\{filename}')
+    shutil.copy(f'{output_file}', f'{production_pc}\{output_file}')
+    shutil.copy(f'{output_file}', f'{onair_pc}\{output_file}')
     os.remove('input.mp3') #remove original file from current directory
-    os.remove(f'{filename}') #remove converted file from current directory
+    os.remove(f'{output_file}') #remove converted file from current directory
 
-def remove_old_files():
-    '''delete yesterday's segments in destination folders'''
-    for old_file in glob.glob(f'{production_pc}\{filename}'):
-        os.remove(old_file)
-    for old_file in glob.glob(f'{onair_pc}\{filename}'):
-        os.remove(old_file)
-    #TODO don't need glob for this, since the filename never changes.
+def download_file():
+    '''download audio file from rss feed'''
+    download = get_audio_url()
+    input_file = 'input.mp3' #name the file we download
+    subprocess.run(f'wget -q -O {input_file} {download}') #using wget because urlretrive is getting a 403 denied error
+    check_downloaded_file(input_file=input_file)
+
+def check_downloaded_file(input_file):
+    filesize = os.path.getsize(input_file)
+    is_not_empty = False
+    i = 0
+    while i < 3:
+        if filesize > 0:
+            convert_copy(input=input_file)
+            is_not_empty = True
+            break
+        else:
+            download_file()
+            i = i+1
+    if is_not_empty == True:
+        pass
+    else:
+        to_send = (f"There was a problem with {show}.\n\n\
+It looks like the downloaded file is empty. Please check manually! \n\n\
+{timestamp}")
+        notify(message=to_send , subject='Error')        
 
 def syslog():
     '''send message to syslog server'''
@@ -102,20 +124,20 @@ def send_sms(message):
 def notify(message, subject):
     weekend = ['Sat', 'Sun']
     if short_day in weekend:
-        send_sms(message) 
-        send_mail(message, subject)
+        send_sms(message=message) 
+        send_mail(message=message, subject=subject)
     else:
-        send_mail(message, subject)
+        send_mail(message=message, subject=subject)
 
-def check_file_exists():
+def check_file_transferred():
     '''check if file transferred to OnAir PC'''
     try: #if file exists, send syslog message
-        os.path.isfile(f'{onair_pc}\{filename}')
+        os.path.isfile(f'{onair_pc}\{output_file}')
         syslog()
     except: #if file doesn't exist, send notification
         to_send = (f"There was a problem with {show}.\n\n\
-    It looks like the file either wasn't converted or didn't transfer correctly. \
-    Please check manually! \n\n\
+It looks like the file either wasn't converted or didn't transfer correctly. \
+Please check manually! \n\n\
     {timestamp}")
         notify(message=to_send, subject='Error')
         os.system('cls')
@@ -126,62 +148,67 @@ def check_file_exists():
 def check_length():
     '''check length of converted file with ffprobe. if too long or short, send notification'''
     duration = subprocess.getoutput(f"ffprobe -v error -show_entries format=duration \
-    -of default=noprint_wrappers=1:nokey=1 {filename}")
+    -of default=noprint_wrappers=1:nokey=1 {output_file}")
     duration = float(duration)
     duration = round(duration)
     duration = duration/60
     
-    if duration > 2.01:
+    if duration > check_if_above:
         to_send = (f"Today's {show} is {duration} minutes long! \
-Please check manually and make edits to bring it below 59 minutes.\n\n\
+Please check manually and make edits to bring it below {check_if_above} minutes.\n\n\
 {timestamp}")
         notify(message=to_send, subject='Check Length')
-    elif duration < 1.99:
+    elif duration < check_if_below:
         to_send = (f"Today's {show} is only {duration} minutes long! \
 This is unusual and could indicate a problem with the file. Please check manually!\n\n\
 {timestamp}")
         notify(message=to_send, subject='Check Length')
     else: pass
 
-def check_feed():
+def get_feed():
     '''check if today's file has been uploaded'''
     header = {'User-Agent': 'Darth Vader'} #requests are denied unless user agent is sent
     req = urllib.request.Request(url, None, header)
     html = urllib.request.urlopen(req)
     html = html.read()
     root = ET.fromstring(html)
-    global feed_updated
-    feed_updated = False
+    return root
+
+def check_feed_updated():
+    root = get_feed()
     for t in root.findall('channel'):
         item = t.find('item') #'find' only returns the first match!
         pub_date = item.find('pubDate').text
-        global download
-        download = item.find('enclosure').attrib
-        download = download.get('url')
         if short_day in pub_date:
             feed_updated = True
+            return feed_updated
+
+def get_audio_url():
+    root = get_feed()
+    for t in root.findall('channel'):
+        item = t.find('item') #'find' only returns the first match!
+        audio_url = item.find('enclosure').attrib
+        audio_url = audio_url.get('url')
+        return audio_url
 
 def check_feed_loop():
     '''for some reason the first time we check the feed, it is not showing as updated.
     It's being cached, or something...? So we are checking it 3 times, for good measure.'''
     i = 0
-    while i <3:
-        check_feed()
+    while i < 3:
+        feed_updated = check_feed_updated()
         if feed_updated == True:
-            break
+            return feed_updated
         else: 
             time.sleep(1)
             i = i+1
 
 #BEGIN
 print(f"I'm working on {show}. Just a moment...")
-check_feed_loop()
-
-# if feed_updated is true, continue. if not, send email.
+feed_updated = check_feed_loop()
 if feed_updated == True:
-    remove_old_files()
-    convert_copy()
-    check_file_exists()
+    download_file()
+    check_file_transferred()
 else:
     to_send = (f"There was a problem with {show}. \n\n\
 It looks like today's file hasn't yet been posted. \
