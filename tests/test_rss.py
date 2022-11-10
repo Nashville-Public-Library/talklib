cbf50fb2cb357f196501c102102bc3add21f21bc
weddleben <stomachworm88@gmail.com>
2022-11-10 15:06:20 -0600

    more tests
    still learning and playing around with testing

--- tests/test_rss.py
+++ tests/test_rss.py
@@ -4,6 +4,8 @@ getting started with some general tests for the talklib module via Pytest.
 import pytest
 
 from talklib.show import TLShow
+import xml.etree.ElementTree as ET
+
 
 # this RSS feed chosen as test feed because it is reliably updated every day 
 # (many times per day) and because it is short!
@@ -25,12 +27,27 @@ def test_attributes():
 
     assert type(test.show) == str
 
-def test_gen2():
-    with pytest.raises(Exception) as b:
-        assert test.run() == b
+def test_decide_whether_to_remove():
+    '''
+    for RSS shows, we always want to remove the source file,
+    which is the file we download from the RSS feed. so this
+    should always be true
+    '''
+    assert test.decide_whether_to_remove()
+
+def test_get_feed():
+    '''check whether return object is an instance of ET.Element class'''
+    assert (isinstance(test.get_feed(), ET.Element))
+
+def test_check_feed_updated():
+    assert test.check_feed_updated()
 
 def test_get_audio_url():
     assert type(test.get_audio_url()) == str
+        
+# def test_gen2():
+#     with pytest.raises(Exception) as b:
+#         assert test.run() == b
 
 def test_teardown():
     '''remove the audio file after the tests have run'''
