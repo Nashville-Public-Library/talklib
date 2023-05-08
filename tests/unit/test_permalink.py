d8053eed6fb34e2c3baf06d4f7be489a30ed8961
weddleben <stomachworm88@gmail.com>
2023-05-08 16:09:40 -0500

    more fixtures (see previous commits)

--- tests/unit/test_permalink.py
+++ tests/unit/test_permalink.py
@@ -4,87 +4,77 @@ from talklib.show import TLShow
 
 url = 'https://pnsne.ws/3mVuTax'
 
-def generate_test_instance():
+@pytest.fixture
+def template():
     test = TLShow()
     test.show = 'Delete Me'
     test.show_filename = 'delete_me'
     test.url = url
     test.is_permalink = True
-    # disable notifications for testing. Need separate tests for these!
+    # disable notifications for templateing. Need separate templates for these!
     test.notifications = False
     test.syslog_enable = False
+
     return test
 
 # ---------- check attributes ----------
 
-def test_attrib_1a():
-    test = generate_test_instance()
-    test.show = None
+def test_attrib_1a(template: TLShow):
+    template.show = None
     with pytest.raises(Exception):
-        test.check_attributes_are_valid()
+        template.check_attributes_are_valid()
 
-def test_attrib_1b():
-    test = generate_test_instance()
-    test.show = 5
+def test_attrib_1b(template: TLShow):
+    template.show = 5
     with pytest.raises(Exception):
-        test.check_attributes_are_valid()
+        template.check_attributes_are_valid()
 
-def test_attrib_1c():
-    test = generate_test_instance()
-    test.show = True
+def test_attrib_1c(template: TLShow):
+    template.show = True
     with pytest.raises(Exception):
-        test.check_attributes_are_valid()
+        template.check_attributes_are_valid()
 
-def test_attrib_1d():
-    test = generate_test_instance()
-    test.show = ''
+def test_attrib_1d(template: TLShow):
+    template.show = ''
     with pytest.raises(Exception):
-        test.check_attributes_are_valid()
+        template.check_attributes_are_valid()
 
-def test_attrib_2a():
-    test = generate_test_instance()
-    test.show_filename = None
+def test_attrib_2a(template: TLShow):
+    template.show_filename = None
     with pytest.raises(Exception):
-        test.check_attributes_are_valid()
+        template.check_attributes_are_valid()
 
-def test_attrib_2b():
-    test = generate_test_instance()
-    test.show_filename = 5
+def test_attrib_2b(template: TLShow):
+    template.show_filename = 5
     with pytest.raises(Exception):
-        test.check_attributes_are_valid()
+        template.check_attributes_are_valid()
 
-def test_attrib_2c():
-    test = generate_test_instance()
-    test.show_filename = True
+def test_attrib_2c(template: TLShow):
+    template.show_filename = True
     with pytest.raises(Exception):
-        test.check_attributes_are_valid()
+        template.check_attributes_are_valid()
 
-def test_attrib_2d():
-    test = generate_test_instance()
-    test.show_filename = ''
+def test_attrib_2d(template: TLShow):
+    template.show_filename = ''
     with pytest.raises(Exception):
-        test.check_attributes_are_valid()
+        template.check_attributes_are_valid()
 
-def test_attrib_3a():
-    test = generate_test_instance()
-    test.url = 5
+def test_attrib_3a(template: TLShow):
+    template.url = 5
     with pytest.raises(Exception):
-        test.check_attributes_are_valid()
+        template.check_attributes_are_valid()
 
-def test_attrib_3b():
-    test = generate_test_instance()
-    test.url = True
+def test_attrib_3b(template: TLShow):
+    template.url = True
     with pytest.raises(Exception):
-        test.check_attributes_are_valid()
+        template.check_attributes_are_valid()
 
-def test_attrib_4b():
-    test = generate_test_instance()
-    test.is_permalink = 5
+def test_attrib_4b(template: TLShow):
+    template.is_permalink = 5
     with pytest.raises(Exception):
-        test.check_attributes_are_valid()
+        template.check_attributes_are_valid()
 
-def test_attrib_4c():
-    test = generate_test_instance()
-    test.is_permalink = 'not boolean'
+def test_attrib_4c(template: TLShow):
+    template.is_permalink = 'not boolean'
     with pytest.raises(Exception):
-        test.check_attributes_are_valid()
+        template.check_attributes_are_valid()
