bbb1271bd1cb70fd109713f46983ca3d95e50d26
weddleben <stomachworm88@gmail.com>
2023-05-08 16:20:14 -0500

    fix hints for fixtures

--- tests/unit/test_rss.py
+++ tests/unit/test_rss.py
@@ -22,7 +22,7 @@ def template():
 
 # ---------- Misc Methods ----------
 
-def test_decide_whether_to_remove(template):
+def test_decide_whether_to_remove(template: TLShow):
     '''
     for RSS shows, we always want to remove the source file,
     which is the file we download from the RSS feed. so this
@@ -30,26 +30,26 @@ def test_decide_whether_to_remove(template):
     '''
     assert template.decide_whether_to_remove()
 
-def test_get_feed(template):
+def test_get_feed(template: TLShow):
     '''check whether return object is an instance of ET.Element class'''
     assert (isinstance(template.get_feed(), ET.Element))
 
-def test_get_feed_fails_with_invalid_url(template):
+def test_get_feed_fails_with_invalid_url(template: TLShow):
     '''check an exception is raised when an invalid url is used'''
     template.url = 'nourl'
     with pytest.raises(Exception):
         template.run()
 
-def test_check_feed_updated(template):
+def test_check_feed_updated(template: TLShow):
     assert template.check_feed_updated()
 
-def test_get_audio_url(template):
+def test_get_audio_url(template: TLShow):
     assert type(template.get_RSS_audio_url()) == str
 
-def test_check_feed_loop(template):
+def test_check_feed_loop(template: TLShow):
     assert type(template.check_feed_loop()) == bool
 
-def test_remove_yesterday_files(template):
+def test_remove_yesterday_files(template: TLShow):
     '''if we pass an invalid file to delete, it should be handled gracefully without exceptions'''
     template.remove(fileToDelete='not_a_file.wav')
 
@@ -58,60 +58,60 @@ def test_remove_yesterday_files(template):
 # ---------- attribute checks ----------
 
 # first, make sure there are no exceptions thrown for our correctly set up instance
-def test_check_attributes_are_valid_1(template):
+def test_check_attributes_are_valid_1(template: TLShow):
     template.check_attributes_are_valid()
 
-def test_gen(template):
+def test_gen(template: TLShow):
     assert type(template.create_output_filename()) == str
 
 # now, start deliberatly triggering exceptions with invalid attributes.
 
-def test_check_attributes_are_valid_1a(template):
+def test_check_attributes_are_valid_1a(template: TLShow):
     template.show = 42
     with pytest.raises(Exception):
         template.check_attributes_are_valid()
 
-def test_check_attributes_are_valid_2(template):
+def test_check_attributes_are_valid_2(template: TLShow):
     template.show_filename = 42
     with pytest.raises(Exception):
         template.check_attributes_are_valid()
 
-def test_check_attributes_are_valid_3(template):
+def test_check_attributes_are_valid_3(template: TLShow):
     template.url = 42
     with pytest.raises(Exception):
         template.check_attributes_are_valid()
 
-def test_check_attributes_are_valid_6(template):
+def test_check_attributes_are_valid_6(template: TLShow):
     template.breakaway = True
     with pytest.raises(Exception):
         template.check_attributes_are_valid()
 
-def test_check_attributes_are_valid_7(template):
+def test_check_attributes_are_valid_7(template: TLShow):
     template.ff_level = True
     with pytest.raises(Exception):
         template.check_attributes_are_valid()
 
-def test_check_attributes_are_valid_8(template):
+def test_check_attributes_are_valid_8(template: TLShow):
     template.check_if_above = [1,2]
     with pytest.raises(Exception):
         template.run()
 
-def test_check_attributes_are_valid_9(template):
+def test_check_attributes_are_valid_9(template: TLShow):
     template.check_if_below = [1,2]
     with pytest.raises(Exception):
         template.run()
 
-def test_check_attributes_are_valid_10(template):
+def test_check_attributes_are_valid_10(template: TLShow):
     template.notifications = 5
     with pytest.raises(Exception):
         template.run()
 
-def test_check_attributes_are_valid_11(template):
+def test_check_attributes_are_valid_11(template: TLShow):
     template.twilio_enable = 4.5
     with pytest.raises(Exception):
         template.run()
  
-def test_check_attributes_are_valid_12(template):
+def test_check_attributes_are_valid_12(template: TLShow):
     '''exception should be raised if both url & is_local are declared'''
     template.is_local = True
     with pytest.raises(Exception):
