829d98baee6a4b83412726970e386f85f06dae72
weddleben <stomachworm88@gmail.com>
2024-01-23 15:20:28 -0600

    issue #30

--- src/talklib/notify.py
+++ src/talklib/notify.py
@@ -11,12 +11,14 @@ class Notify:
     def __init__ (self,
                   syslog_enable: bool = True,
                   twilio_enable: bool = True,
-                  email_enable: bool = True
+                  email_enable: bool = True,
+                  EV = EV()
                   ):
         
         self.syslog_enable = syslog_enable
         self.twilio_enable = twilio_enable
         self.email_enable = email_enable
+        self.EV = EV
 
     def send_syslog(self, message: str) -> None:
         '''send message to syslog server'''
@@ -24,7 +26,7 @@ class Notify:
             port = int('514')
             my_logger = logging.getLogger('MyLogger')
             my_logger.setLevel(logging.DEBUG)
-            handler = SysLogHandler(address=(EV().syslog_host, port))
+            handler = SysLogHandler(address=(self.EV.syslog_host, port))
             my_logger.addHandler(handler)
 
             my_logger.info(message)
@@ -33,23 +35,23 @@ class Notify:
     def send_call(self, message: str) -> None:
         '''send voice call via twilio'''
         if self.twilio_enable:
-            client = Client(EV().twilio_sid, EV().twilio_token)
+            client = Client(self.EV.twilio_sid, self.EV.twilio_token)
 
             call = client.calls.create(
                                     twiml=f'<Response><Say>{message}</Say></Response>',
-                                    to=EV().twilio_to,
-                                    from_=EV().twilio_from
+                                    to=self.EV.twilio_to,
+                                    from_=self.EV.twilio_from
                                 )
             call.sid
 
     def send_sms(self, message: str) -> None:
         '''send sms via twilio. '''
         if self.twilio_enable:
-            client = Client(EV().twilio_sid, EV().twilio_token)
+            client = Client(self.EV.twilio_sid, self.EV.twilio_token)
             SMS = client.messages.create(
                 body=message,
-                from_=EV().twilio_from,
-                to=EV().twilio_to
+                from_=self.EV.twilio_from,
+                to=self.EV.twilio_to
             )
             SMS.sid
 
@@ -59,9 +61,9 @@ class Notify:
             format = EmailMessage()
             format.set_content(message)
             format['Subject'] = subject
-            format['From'] = EV().fromEmail
-            format['To'] = EV().toEmail
+            format['From'] = self.EV.fromEmail
+            format['To'] = self.EV.toEmail
 
-            mail = smtplib.SMTP(host=EV().mail_server)
+            mail = smtplib.SMTP(host=self.EV.mail_server)
             mail.send_message(format)
             mail.quit()
