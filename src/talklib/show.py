497977e6fdf76c8ab19b7c2a97404d0b6c115f3a
weddleben <stomachworm88@gmail.com>
2024-02-12 16:28:47 -0600

    more of the same!
    (see previous commit)

--- src/talklib/show.py
+++ src/talklib/show.py
@@ -44,7 +44,7 @@ class TLShow():
     def create_output_filename(self) -> str:
         '''returns the audio filename to use depending on whether we should include the date'''
         if self.include_date:
-            date = TLShow.six_digit_date_string(self)
+            date = self.six_digit_date_string()
             outputFile = (f"{self.show_filename}-{date}.wav")
         else:
             outputFile = (f'{self.show_filename}.wav')
@@ -71,11 +71,11 @@ class TLShow():
         '''TODO explain'''
         
         for destination in self.destinations:
-            TLShow.prep_syslog(self, message=f'Copying {fileToCopy} to {destination}...')
+            self.prep_syslog(message=f'Copying {fileToCopy} to {destination}...')
             shutil.copy(fileToCopy, destination)
 
         #this is the file we're copying, so it is the file already converted. we always want to remove this.
-        TLShow.remove(self, fileToDelete=fileToCopy, is_output_file=True)
+        self.remove(fileToDelete=fileToCopy, is_output_file=True)
 
     def decide_whether_to_remove(self) -> bool:
         '''
@@ -96,7 +96,7 @@ class TLShow():
     def remove(self, fileToDelete, is_output_file: bool=False):
         '''TODO explain'''
         if TLShow.decide_whether_to_remove(self) or is_output_file:
-            TLShow.prep_syslog(self, message=f'Deleting {fileToDelete}')
+            self.prep_syslog(message=f'Deleting {fileToDelete}')
             try:
                 os.remove(fileToDelete)
             except Exception as e:
@@ -112,10 +112,10 @@ class TLShow():
                 matched_filenames = glob.glob(f'{destination}/{self.show_filename}*.wav')
                 if matched_filenames:
                     for file in matched_filenames:
-                        TLShow.prep_syslog(self, message=f'Deleting existing matched file: {file}')
+                        self.prep_syslog(message=f'Deleting existing matched file: {file}')
                         os.remove(f'{file}')
                 else:
-                    TLShow.prep_syslog(self, message=f"{self.show}: Cannot find existing matched files in {destination}. Continuing...")
+                    self.prep_syslog(message=f"{self.show}: Cannot find existing matched files in {destination}. Continuing...")
 
     def download_file(self):
         '''
@@ -132,16 +132,16 @@ class TLShow():
         if self.is_permalink:
             download_URL = self.url
         else:
-            download_URL = TLShow.get_RSS_audio_url(self)
+            download_URL = self.get_RSS_audio_url()
 
-        TLShow.prep_syslog(self, message=f'Attempting to download audio file.')
+        self.prep_syslog(message=f'Attempting to download audio file.')
         input_file = 'input.mp3'  # name the file we download
         with open (input_file, mode='wb') as downloaded_file:
             a = requests.get(download_URL)
             downloaded_file.write(a.content)
             downloaded_file.close()
         
-        TLShow.prep_syslog(self, message=f'File downloaded successfully in {os.getcwd()}.')
+        self.prep_syslog(message=f'File downloaded successfully in {os.getcwd()}.')
         return downloaded_file.name
 
     def check_downloaded_file(self, fileToCheck, how_many_attempts):
@@ -149,28 +149,28 @@ class TLShow():
         try:
             filesize = os.path.getsize(fileToCheck)
         except FileNotFoundError as error:
-            TLShow.send_notifications(self, message=f'It looks like the file does not exist. Here is the error: {error}', subject='Error')
+            self.send_notifications(message=f'It looks like the file does not exist. Here is the error: {error}', subject='Error')
             raise FileNotFoundError
             
         is_not_empty = False
         while how_many_attempts < 3:
             if filesize > 0:
-                TLShow.prep_syslog(self, message='File is not empty. Continuing...')
+                self.prep_syslog(message='File is not empty. Continuing...')
                 is_not_empty = True
                 return True
             else:
-                TLShow.prep_syslog(self, message=f'File is empty. Will download again. Attempt # {how_many_attempts}.')
+                self.prep_syslog(message=f'File is empty. Will download again. Attempt # {how_many_attempts}.')
                 how_many_attempts = how_many_attempts+1
-                TLShow.download_file(self)
-                TLShow.check_downloaded_file(self, fileToCheck=fileToCheck, how_many_attempts=how_many_attempts)
+                self.download_file()
+                self.check_downloaded_file(fileToCheck=fileToCheck, how_many_attempts=how_many_attempts)
         if not is_not_empty:
             toSend = (
 f"There was a problem with {self.show}.\n\n\
 It looks like the downloaded file is empty. Please check manually! Yesterday's file will remain.\n\n\
 {get_timestamp()}"
 )
-            TLShow.send_notifications(self, message=toSend, subject='Error')
-            TLShow.remove(self, fileToDelete=fileToCheck)
+            self.send_notifications(message=toSend, subject='Error')
+            self.remove(fileToDelete=fileToCheck)
             raise Exception (toSend)
             
     def prep_syslog(self, message: str):
@@ -191,12 +191,12 @@ It looks like the downloaded file is empty. Please check manually! Yesterday's f
     def send_notifications(self, message: str, subject: str):
         '''we generally only want to send SMS via Twilio if today is on a weekend'''
         if today_is_weekday():
-            TLShow.prep_send_mail(self, message=message, subject=subject)
-            TLShow.prep_syslog(self, message=message)
+            self.prep_send_mail(message=message, subject=subject)
+            self.prep_syslog(message=message)
         else:
-            TLShow.send_sms_if_enabled(self, message=message)
-            TLShow.prep_send_mail(self, message=message, subject=subject)
-            TLShow.prep_syslog(self, message=message)
+            self.send_sms_if_enabled(message=message)
+            self.prep_send_mail(message=message, subject=subject)
+            self.prep_syslog(message=message)
 
     def check_file_transferred(self, fileToCheck):
         '''check if file transferred to destination(s)'''
@@ -205,18 +205,18 @@ It looks like the downloaded file is empty. Please check manually! Yesterday's f
         while numberOfDestinations >= 0:
             if os.path.isfile(f'{self.destinations[numberOfDestinations]}/{fileToCheck}'):
                 numberOfDestinations = numberOfDestinations-1
-                TLShow.prep_syslog(self, message=f'{fileToCheck} arrived at {self.destinations[numberOfDestinations]}')
+                self.prep_syslog(message=f'{fileToCheck} arrived at {self.destinations[numberOfDestinations]}')
                 success = True
             else:
                 toSend = (f"There was a problem with {self.show}.\n\n\
 It looks like the file either wasn't converted or didn't transfer correctly. \
 Please check manually!\n\n\
 {get_timestamp()}")
-                TLShow.send_notifications(self, message=toSend, subject='Error')
+                self.send_notifications(message=toSend, subject='Error')
                 print_to_screen_and_wait(message=toSend)
                 break
         if success:
-            TLShow.countdown(self)
+            self.countdown()
 
     def check_length(self, fileToCheck):
         '''
@@ -226,9 +226,9 @@ Please check manually!\n\n\
         '''
         # if these are not declared, don't run this check.
         if not (self.check_if_below and self.check_if_above):
-            TLShow.prep_syslog(self, message='The check length function is turned off.')
+            self.prep_syslog(message='The check length function is turned off.')
         else:
-            TLShow.prep_syslog(self, message=f'Checking whether length is between \
+            self.prep_syslog(message=f'Checking whether length is between \
 {self.check_if_below} and {self.check_if_above}')
 
             duration = FFMPEG(input_file=fileToCheck).get_length_in_minutes()
@@ -237,16 +237,16 @@ Please check manually!\n\n\
                 toSend = (f"Today's {self.show} is {duration} minutes long! \
 Please check manually and make edits to bring it below {self.check_if_above} minutes.\n\n\
 {get_timestamp()}")
-                TLShow.send_notifications(self, message=toSend, subject='Check Length')
+                self.send_notifications(message=toSend, subject='Check Length')
 
             elif duration < self.check_if_below:
                 toSend = (f"Today's {self.show} is only {duration} minutes long! \
 This is unusual and could indicate a problem with the file. Please check manually!\n\n\
 {get_timestamp()}")
-                TLShow.send_notifications(self, message=toSend, subject='Check Length')
+                self.send_notifications(message=toSend, subject='Check Length')
 
             else:
-                TLShow.prep_syslog(self, message=f'File is {duration} minute(s). Continuing...')
+                self.prep_syslog(message=f'File is {duration} minute(s). Continuing...')
             
             return duration
 
@@ -265,7 +265,7 @@ Here's the error: {a}\n\n\
 Is this a permalink show? Did you forget to set the is_permalink attribute?\n\n\
 {get_timestamp()}"
                 )
-            TLShow.send_notifications(self, subject='Error', message=to_send)
+            self.send_notifications(subject='Error', message=to_send)
             raise Exception (a)
 
     def check_feed_updated(self) -> bool:
@@ -277,23 +277,23 @@ Is this a permalink show? Did you forget to set the is_permalink attribute?\n\n\
         The format for this date is a standard format (E.G. '17 Oct 2022') set by 
         a standards organization. Most podcasts/RSS feeds follow this standard.
         '''
-        root = TLShow.get_feed(self)
+        root = self.get_feed()
         for t in root.findall('channel'):
             item = t.find('item')  # 'find' only returns the first match!
             pub_date = item.find('pubDate').text
             today = datetime.now().strftime("%a, %d %b %Y")
             if today in pub_date:
-                TLShow.prep_syslog(self, message='The feed is updated.')
+                self.prep_syslog(message='The feed is updated.')
                 return True
 
     def get_RSS_audio_url(self) -> str:
         '''TODO: explain'''
-        root = TLShow.get_feed(self)
+        root = self.get_feed()
         for t in root.findall('channel'):
             item = t.find('item')  # 'find' only returns the first match!
             audio_url = item.find('enclosure').attrib
             audio_url = audio_url.get('url')
-            TLShow.prep_syslog(self, message=f'Audio URL is: {audio_url}')
+            self.prep_syslog(message=f'Audio URL is: {audio_url}')
             return audio_url
 
     def check_feed_loop(self) -> str:
@@ -304,8 +304,8 @@ Is this a permalink show? Did you forget to set the is_permalink attribute?\n\n\
         i = 0
         feed_updated = False
         while i < 3:
-            TLShow.prep_syslog(self, message=f'Attempt {i} to check feed.')
-            feed_updated = TLShow.check_feed_updated(self)
+            self.prep_syslog(message=f'Attempt {i} to check feed.')
+            feed_updated = self.check_feed_updated()
             if feed_updated:
                 break
             else:
