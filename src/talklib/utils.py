1b8e079b138738476fb0f7942d053da0059248c5
weddleben <stomachworm88@gmail.com>
2024-04-02 14:42:54 -0500

    first attempt at issue #27

--- src/talklib/utils.py
+++ src/talklib/utils.py
@@ -1,6 +1,10 @@
 from datetime import datetime
 import os
 
+import requests
+
+from talklib.notify import Notify
+
 def get_timestamp() -> str:
     timestamp = datetime.now().strftime('%H:%M:%S on %d %b %Y')
     return timestamp
@@ -26,3 +30,23 @@ def today_is_weekday() -> bool:
         return True
     else:
         return False
+    
+def metadata_to_icecast(title):
+    notify = Notify()
+    notify.syslog.send_syslog_message(message=f'attempting to send "{title}" to Icecast')
+    user = os.environ['icecast_user']
+    password = os.environ['icecast_pass']
+    url = f'https://npl.streamguys1.com:80/admin/metadata?mount=/live&mode=updinfo&song={title}'
+    send = requests.get(url, auth = (user, password))
+    if send.status_code == 200:
+        notify.syslog.send_syslog_message(message=f'Successfully sent "{title}" to Icecast') 
+        return
+    
+    to_send = f'There was a problem sending metadata ({title}) to Icecast. The response code was: {send.status_code}'
+    if today_is_weekday():
+        notify.syslog.send_syslog_message(message=to_send, level='error')
+        notify.send_mail(subject='Error', message=to_send)
+    else:
+        notify.syslog.send_syslog_message(message=to_send, level='error')
+        notify.send_mail(subject='Error', message=to_send)
+        notify.send_sms(message=to_send)
