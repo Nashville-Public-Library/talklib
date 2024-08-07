92d020f4c9c63ca6b96bf6fcddef4fef4cfe30c4
weddleben <stomachworm88@gmail.com>
2024-08-07 16:43:24 -0500

    update tests due to pydantic

--- src/tests/unit/test_rss.py
+++ src/tests/unit/test_rss.py
@@ -15,10 +15,11 @@ url = 'https://feeds.npr.org/500005/podcast.xml'
 @pytest.fixture
 def template_rss():
     with patch.dict('os.environ', env_vars):
-        test = TLShow()
-    test.show = 'Delete Me'
-    test.show_filename = 'delete_me'
-    test.url = url
+        test = TLShow(
+            show = 'Delete Me',
+            show_filename = 'delete_me',
+            url = url
+        )
     # disable notifications for testing. Need separate tests for these!
     test.notifications.enable_all = False
 
@@ -45,32 +46,3 @@ def test_remove_yesterday_files(template_rss: TLShow):
 
 def test_gen(template_rss: TLShow):
     assert type(template_rss._TLShow__create_output_filename()) == str
-
-# now, start deliberately triggering exceptions with invalid attributes.
-
-def test_check_attributes_are_valid_8(template_rss: TLShow):
-    template_rss.check_if_above = [1,2]
-    with pytest.raises(Exception):
-        template_rss.run()
-
-def test_check_attributes_are_valid_9(template_rss: TLShow):
-    template_rss.check_if_below = [1,2]
-    with pytest.raises(Exception):
-        template_rss.run()
-
-def test_check_attributes_are_valid_10(template_rss: TLShow):
-    template_rss.notifications.email_enable = 5
-    with pytest.raises(Exception):
-        template_rss.run()
-
-def test_check_attributes_are_valid_11(template_rss: TLShow):
-    template_rss.notifications.syslog_enable = 4.5
-    with pytest.raises(Exception):
-        template_rss.run()
- 
-def test_check_attributes_are_valid_12(template_rss: TLShow):
-    '''exception should be raised if both url & is_local are declared'''
-    template_rss.is_local = True
-    with pytest.raises(Exception):
-        template_rss.run()
-
