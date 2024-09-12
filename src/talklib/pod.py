db9df5d22110b8f872e1fff8008fc349f0fb0c6c
Ben Weddle <107364642+weddleben@users.noreply.github.com>
2024-09-12 18:39:48 -0500

    progress...

--- src/talklib/pod.py
+++ src/talklib/pod.py
@@ -1,4 +1,5 @@
-
+import boto3
+import os
 from typing import Type
 
 from pydantic import BaseModel, Field
@@ -6,6 +7,35 @@ from pydantic import BaseModel, Field
 from talklib.ev import EV
 from talklib.notify import Notify
 from talklib.ffmpeg import FFMPEG
+
+class AWS():
+    s3 = boto3.client(
+    's3', 
+    aws_access_key_id=os.environ["AWS_ACCESS_KEY_ID"], 
+    aws_secret_access_key=os.environ["AWS_SECRET_ACCESS_KEY"]
+    )
+    
+    def print_buckets(self):
+    # Retrieve the list of existing buckets
+        response = self.s3.list_buckets()
+        response = response['Buckets']
+        for bucket_name in response:
+            print(bucket_name["Name"])
+
+    def print_folders(self):
+        result = self.s3.list_objects(Bucket="tlpod")
+        for contents in result["Contents"]:
+            key:str = contents["Key"]
+            if key.endswith("/"):
+                print(key)
+    
+    def print_files(self):
+        result = self.s3.list_objects(Bucket="tlpod")
+        for contents in result["Contents"]:
+            key:str = contents["Key"]
+            if not key.endswith("/"):
+                print(key)
+
 
 class TLPod(BaseModel):
     show: str = Field(default=None)
--- src/tests/pod/test_pod.py
+++ src/tests/pod/test_pod.py
@@ -6,12 +6,12 @@ import pytest
 from .mock import env_vars
 with patch.dict('os.environ', env_vars):
     from talklib import TLPod
+    from talklib.pod import AWS
 
 
 def test_type_1():
     '''pydantic should raise an error if wrong type passed in'''
     with pytest.raises(ValidationError):
-        with patch.dict('os.environ', env_vars):
             test = TLPod(
                 show=5, 
                 show_filename="del",
@@ -20,7 +20,6 @@ def test_type_1():
 def test_type_2():
     '''pydantic should raise an error if wrong type passed in'''
     with pytest.raises(ValidationError):
-        with patch.dict('os.environ', env_vars):
             test = TLPod(
                 show=5, 
                 show_filename=5,
@@ -29,8 +28,13 @@ def test_type_2():
 def test_type_3():
     '''pydantic should raise an error if wrong type passed in'''
     with pytest.raises(ValidationError):
-        with patch.dict('os.environ', env_vars):
             test = TLPod(
                 show='delete', 
                 show_filename="del",
                 bucket_folder=5)
+            
+def test_buckets():
+    a = AWS()
+    a.print_buckets()
+    a.print_folders()
+    a.print_files()
