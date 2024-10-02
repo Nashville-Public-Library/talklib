71e13229a4db1f974c4cc097408c52942d17dd10
Ben Weddle <107364642+weddleben@users.noreply.github.com>
2024-10-02 13:52:06 -0500

    add baisc validation tests, modify existing tests slightly

--- src/tests/pod/test_pod.py
+++ src/tests/pod/test_pod.py
@@ -13,30 +13,56 @@ with patch.dict('os.environ', env_vars):
     from talklib.pod import AWS
 
 
-
 def test_type_1():
-    '''pydantic should raise an error if wrong type passed in'''
+    '''pydantic should raise an error if instance variables aren't set'''
     with pytest.raises(ValidationError):
-            test = TLPod(
-                display_name=5, 
-                show_filename="del",
-                bucket_folder='nope')
-            
+            test = TLPod()
+
 def test_type_2():
     '''pydantic should raise an error if wrong type passed in'''
     with pytest.raises(ValidationError):
             test = TLPod(
                 display_name=5, 
-                show_filename=5,
-                bucket_folder='nope')
-
+                filename_to_match="del")
+            
 def test_type_3():
     '''pydantic should raise an error if wrong type passed in'''
     with pytest.raises(ValidationError):
             test = TLPod(
+                display_name='Some Name', 
+                filename_to_match=5)
+
+def test_type_4():
+    '''pydantic should raise an error if wrong type passed in'''
+    with pytest.raises(ValidationError):
+            test = TLPod(
                 display_name='delete', 
-                show_filename="del",
+                filename_to_match="del",
                 bucket_folder=5)
+            
+def test_type_5():
+    '''pydantic should raise an error if wrong type passed in'''
+    with pytest.raises(ValidationError):
+            test = TLPod(
+                display_name='delete', 
+                filename_to_match="del",
+                max_episodes_in_feed=3.1)
+            
+def test_type_6():
+    '''pydantic should raise an error if wrong type passed in'''
+    with pytest.raises(ValidationError):
+            test = TLPod(
+                display_name='delete', 
+                filename_to_match="del",
+                filename_override=5)
+            
+def test_bucket_filename_match():
+          '''if no bucket folder is set explicitly, bucket folder should be set to filename_to_match'''
+          test = TLPod(
+               display_name='testing',
+               filename_to_match = 'testing123'
+          )
+          assert test.bucket_folder == test.filename_to_match
             
 def test_match_file_1():
     '''checking matched filename'''
