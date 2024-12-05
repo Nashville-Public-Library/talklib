f7e6d60a2e2252f622e406001b639fb767ef7e75
weddleben <stomachworm88@gmail.com>
2024-12-05 15:15:58 -0600

    move above fixture

--- conftest.py
+++ conftest.py
@@ -2,10 +2,10 @@ import pytest
 
 from src.tests.mock import env_vars
 
+pytest_plugins = ["pytester"]
+
 @pytest.fixture(autouse=True)
 def mock_env_vars(monkeypatch):
     # Mock the 'destinations' environment variable globally
     for key, value in env_vars.items():
         monkeypatch.setenv(key, value)
-
-pytest_plugins = ["pytester"]
