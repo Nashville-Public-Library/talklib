cc0be6ab18ec2dfb7611f0fd2e735296685e9f83
weddleben <stomachworm88@gmail.com>
2024-12-05 14:35:41 -0600

    pretty sure this will fail too...

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -2,7 +2,7 @@ import pytest
 
 from .mock import env_vars
 
-@pytest.fixture(autouse=True, scope="function")
+@pytest.fixture(autouse=True)
 def mock_env_vars(monkeypatch):
     # Mock the 'destinations' environment variable globally
     for key, value in env_vars.items():
