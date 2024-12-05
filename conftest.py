dea324e2eb6d6f660dc07318b44a7e4152680311
weddleben <stomachworm88@gmail.com>
2024-12-05 15:21:43 -0600

    MAYBE this will work

--- conftest.py
+++ conftest.py
@@ -1,11 +1,9 @@
+import os
+
 import pytest
 
 from src.tests.mock import env_vars
 
-pytest_plugins = ["pytester"]
-
-@pytest.fixture(autouse=True)
-def mock_env_vars(monkeypatch):
-    # Mock the 'destinations' environment variable globally
+def pytest_sessionstart(session):
     for key, value in env_vars.items():
-        monkeypatch.setenv(key, value)
+        os.environ[key] = value
