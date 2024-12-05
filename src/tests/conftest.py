c250dba6c87e5dd973b7ce99fc386f8ca12ba3e9
weddleben <stomachworm88@gmail.com>
2024-12-05 14:29:47 -0600

    trying to get this...

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -2,7 +2,7 @@ import pytest
 
 from .mock import env_vars
 
-@pytest.fixture(autouse=True)
+@pytest.fixture(autouse=True, scope="function")
 def mock_env_vars(monkeypatch):
     # Mock the 'destinations' environment variable globally
     for key, value in env_vars.items():
