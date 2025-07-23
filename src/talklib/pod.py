ad85c643ca00c09a2395facb4f8b552e1dd5310f
Ben Weddle <107364642+weddleben@users.noreply.github.com>
2025-07-23 16:11:56 -0500

    prefer False as default

--- src/talklib/pod.py
+++ src/talklib/pod.py
@@ -360,7 +360,7 @@ class TLPod(BaseModel):
     bucket_folder: str = Field(default=None)
     max_episodes_in_feed: int = Field(ge=1, default=5)
     override_filename: bool = False
-    include_date: bool = True
+    short_timestamp: bool = False
     audio_folders:list = EV().destinations
     notifications: Type[Notifications] = Notifications()
     episode: Type[Episode] = Episode()
@@ -382,10 +382,10 @@ class TLPod(BaseModel):
         prefix = f"{self.display_name} (podcast)"
         Notifications.prefix = prefix
 
-        if self.include_date:
-            self.display_name = f"{self.display_name} ({datetime.now().strftime('%a, %d %b')})"
+        if self.short_timestamp:
+            self.display_name = f"{self.display_name} ({self.short_timestamp_for_display_name()})"
         else:
-            self.display_name = f"{self.display_name} ({self.generate_timestamp_for_display_name()})"
+            self.display_name = f"{self.display_name} ({datetime.now().strftime('%a, %d %b')})"
 
         return self
     
@@ -400,7 +400,7 @@ class TLPod(BaseModel):
     #         raise e
 
     #     return self
-    def generate_timestamp_for_display_name(self):
+    def short_timestamp_for_display_name(self):
             day_of_week: str = datetime.now().strftime("%A") # Weekday as locale’s full name.
             hour: str = datetime.now().strftime('%I') # Hour (12-hour clock) as a zero-padded decimal number.
             ampm: str = datetime.now().strftime("%p") # Locale’s equivalent of either AM or PM.
