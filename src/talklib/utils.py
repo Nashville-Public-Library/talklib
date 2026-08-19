d3436a2e6af34c6d6c6e4c5b1d3a30c901686dcc
Ben Weddle <107364642+weddleben@users.noreply.github.com>
2026-08-19 15:57:34 -0500

    temporarily send metadata to both icecast servers

--- src/talklib/utils.py
+++ src/talklib/utils.py
@@ -27,16 +27,25 @@ def today_is_weekday() -> bool:
     
 def metadata_to_icecast(title):
     notify = Notify()
-    notify.syslog.send_syslog_message(message=f'attempting to send "{title}" to Icecast')
     user = os.environ['icecast_user']
     password = os.environ['icecast_pass']
-    url = f'https://npl.streamguys1.com:80/admin/metadata?mount=/live&mode=updinfo&song={title}'
-    send = requests.get(url, auth = (user, password))
-    if send.status_code == 200:
-        notify.syslog.send_syslog_message(message=f'Successfully sent "{title}" to Icecast') 
-        return
-    
-    to_send = f'There was a problem sending metadata ({title}) to Icecast. The response code was: {send.status_code}'
+    URLs: tuple = (f'https://npl.streamguys1.com/admin/metadata?mount=/live&mode=updinfo&song={title}', 
+                   f"https://stream.talkinglibrary.nashville.gov/admin/metadata?mount=/live_64_intro.mp3&mode=updinfo&song={title}")
+    for url in URLs:
+        try:
+            notify.syslog.send_syslog_message(message=f'attempting to send "{title}" to {url}')
+            send = requests.get(url, auth = (user, password))
+            if send.status_code == 200:
+                notify.syslog.send_syslog_message(message=f'Successfully sent "{title}" to {url}') 
+                continue
+            else: notify_on_metadata_problem(title=title, url=url, error=send.status_code)
+            
+        except Exception as e:
+            notify_on_metadata_problem(title=title, url=url, error=e)
+
+def notify_on_metadata_problem(title: str, url: str, error: str | int):
+    notify = Notify()
+    to_send = f'There was a problem sending metadata ({title}) to {url}: {error}'
     if today_is_weekday():
         notify.syslog.send_syslog_message(message=to_send, level='error')
         notify.send_mail(subject='Error', message=to_send)
